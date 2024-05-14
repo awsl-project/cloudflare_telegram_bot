1763743e54f0f8cc2c0a81c71ad4604726e4253a
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-05-14 22:11:00 +0800

    init

--- src/telegram.ts
+++ src/telegram.ts
@@ -3,11 +3,38 @@ import { Telegraf, Context as TgContext } from "telegraf";
 import { message } from "telegraf/filters";
 import { Context } from "hono";
 
+const COMMANDS = [
+    {
+        command: "start",
+        description: "开始使用"
+    },
+    {
+        command: "awsl",
+        description: "awsl 随机一图"
+    },
+    {
+        command: "moyuban",
+        description: "摸鱼办提醒"
+    },
+    {
+        command: "mjx",
+        description: "买家秀随机一图"
+    },
+    {
+        command: "chatgpt",
+        description: "和AI聊天, 例如 /chatgpt 你好"
+    }
+]
+
 
 export function newTelegramBot(c: Context, token: string): Telegraf {
     const bot = new Telegraf(token);
     bot.command("ping", ctx => ctx.reply("pong"))
-    bot.command("start", ctx => ctx.reply("type command se to use"))
+
+    bot.command("start", ctx => ctx.reply(
+        "欢迎使用本机器人\n\n" +
+        COMMANDS.map(c => `/${c.command}: ${c.description}`).join("\n")
+    ))
 
     const send_awsl = async (ctx: TgContext) => {
         const res = await fetch(`${c.env.API_URL}/v2/random`)
@@ -49,6 +76,15 @@ export function newTelegramBot(c: Context, token: string): Telegraf {
         return result
     }
 
+    bot.command("chatgpt", async (ctx: TgContext) => {
+        // @ts-ignore
+        const prompt = ctx?.message?.text.slice("/chatgpt".length).trim();
+        if (!prompt || prompt.length === 0) {
+            return await ctx.reply("请输入聊天内容")
+        }
+        return await aicommand(ctx, prompt)
+    });
+
     bot.on(message('text'), async (ctx: TgContext) => {
         // @ts-ignore
         if (!ctx?.message?.text?.startsWith(c.env.BOT_NAME)) {
@@ -60,6 +96,10 @@ export function newTelegramBot(c: Context, token: string): Telegraf {
             console.error("Empty prompt")
             return
         }
+        return await aicommand(ctx, prompt)
+    });
+
+    const aicommand = async (ctx: TgContext, prompt: string) => {
         try {
             const response = await openai_chat(prompt)
             const reply = response?.choices[0]?.message?.content
@@ -78,24 +118,11 @@ export function newTelegramBot(c: Context, token: string): Telegraf {
             return
         }
         return await ctx.reply(reply)
-    })
+    }
 
     return bot;
 }
 
 export async function initTelegramBotCommands(bot: Telegraf) {
-    await bot.telegram.setMyCommands([
-        {
-            command: "awsl",
-            description: "awsl 随机一图"
-        },
-        {
-            command: "moyuban",
-            description: "摸鱼办提醒"
-        },
-        {
-            command: "mjx",
-            description: "买家秀随机一图"
-        },
-    ]);
+    await bot.telegram.setMyCommands(COMMANDS);
 }
